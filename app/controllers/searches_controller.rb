db6f15cd80ecb20a8dd90d2e879818795b87fff7
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-12-23 06:25:54 +0000

    レイアウト調整

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,11 @@
 class SearchesController < ApplicationController
-#validates :content, presence: true
 	def search
-		#@user = User.find(params[:id])
 		@model = params[:model]
 		@content = params[:content]
 		@method = params[:method]
 		unless @content.present?#contentの中が入っているかを確認する
 			if request.referer.present?
 				redirect_to request.referer, notice: "検索ワードを入力してください"#入っていなければ元のページに戻る
-			#else
-				#redirect_to root_path
 			end
 		end
 		if @model == 'user'
@@ -18,6 +14,4 @@ class SearchesController < ApplicationController
 			@records = Info.search_for(@content, @method).page(params[:page]).per(15)
 		end
 	end
-	#attr_accessor :content
-  #validates :content, presence: true
 end
