97a2caabff4964424a9b1bec4ff71ae1d9cc98ef
ruisunene <ruisunene@gmail.com>
2023-01-15 11:28:39 +0000

    コメントアウトの追加・サイドバーを部分テンプレート化

--- app/controllers/public/infos_controller.rb
+++ app/controllers/public/infos_controller.rb
@@ -1,9 +1,8 @@
+#ユーザー側の情報の投稿に関するコントローラー
 class Public::InfosController < ApplicationController
   before_action :authenticate_user!
   #ログインユーザーのみ投稿の編集と削除ができる
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
-
-#投稿機能
 
   def new
     @info = Info.new
@@ -11,7 +10,7 @@ class Public::InfosController < ApplicationController
 
   def create
     @info = Info.new(info_params)
-    @info.user_id = current_user.id
+    @info.user_id = current_user.id #投稿とユーザーを紐付け
     if @info.save
       redirect_to info_path(@info), notice: "投稿に成功しました"
     else
@@ -21,17 +20,18 @@ class Public::InfosController < ApplicationController
 
   def index
     @genres = Genre.all
-    @bookmarks = Bookmark.where(user_id: current_user.id)
+    @bookmarks = Bookmark.where(user_id: current_user.id)#ブックマークの表示をログインしているユーザーに紐付け
     #ジャンルの検索結果を抽出
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
       @info = Info.find_by(params[:info_id])
-      @infos = @genre.infos.page(params[:page]).per(15).order(created_at: :desc)
+      @infos = @genre.infos.page(params[:page]).per(15).order(created_at: :desc)#1ページの表示を15個までし、新しい投稿が上に表示される
+    #サーチでの検索結果を摘出
     elsif @search_infos
       @info = Info.find_by(params[:info_id])
       @infos = @search_infos.page(params[:page]).per(15).order(created_at: :desc)
     else
-      #投稿を抽出
+      #通常の投稿を抽出
       @info = Info.find_by(params[:info_id])
       @infos = Info.page(params[:page]).per(15).order(created_at: :desc)
     end
@@ -69,7 +69,7 @@ class Public::InfosController < ApplicationController
     def info_params
       params.require(:info).permit(:title, :body, :genre_id)
     end
-
+# 投稿に紐づいているユーザーと現在ログインしているユーザーか確認
     def ensure_correct_user
       @info = Info.find(params[:id])
       unless @info.user == current_user
